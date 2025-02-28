acaf1ca3cfbc15f11c6f09f9e640eb09e47e56b9
flori <florianpoegl80@gmail.com>
2025-02-28 19:51:35 +0100

    created classes

---
+++ AOS_TP/node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOS_TP
+{
+    internal class node
+    {
+    }
+}
